49519ef791db18b067b9b2c4ff0eba32b63dcea6
Mäx <max.kiehnscherf@gmail.com>
2018-08-19 04:14:51 +0200

    [wip] experiment with encryption

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 extern crate serde_yaml;
+extern crate crypto;
+extern crate rand;
+extern crate base64;
 
-use std::fs::File;
-use std::io::prelude::*;
+use rand::prelude::*;
+use rand::distributions::Alphanumeric;
+use base64::{encode, decode};
 
 fn main() {
-    let mut file = File::open("./debug.yml").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-
-    let mut yml: serde_yaml::Value = serde_yaml::from_str(&contents).unwrap();
-    let path = "services.whoami.environment.DB_PASSWORD"
-        .split('.')
-        .collect();
-    yaml::replace_value(&mut yml, path, String::from("lol"));
-    println!("{}", serde_yaml::to_string(&yml).unwrap())
+  let mut rng = thread_rng();
+  let iv: String = rng.sample_iter(&Alphanumeric).take(16).collect();
+  match aes::encrypt("geheim".to_owned().as_bytes(), "password".to_owned().as_bytes(), iv.as_bytes()) {
+    Ok(a) => {
+      let crypted = encode(a.as_slice());
+      assert_eq!(decode(&crypted).unwrap(), a);
+      println!("success!");
+    },
+    Err(_) => println!("something's fishy")
+  };
 }
 
 mod yaml;
+mod aes;
