4e6744badd8f65ca36df32d8c3f567abeff9fa54
Mäx <max.kiehnscherf@gmail.com>
2018-09-27 17:59:43 +0200

    [refactor] extract clap app to own function

--- src/main.rs
+++ src/main.rs
@@ -11,49 +11,54 @@ use std::io::{stdin, stdout, Write};
 use std::path::Path;
 use std::str;
 use vault::Vault;
+fn app<'a>() -> App<'a,'a> {
+  App::new("culper")
+      .version("0.1.0")
+      .author("Max Kiehnscherf")
+      .about("Embed crypted values in your yaml")
+      .arg(
+          Arg::with_name("file")
+              .short("f")
+              .long("file")
+              .help("Sets input file")
+              .takes_value(true)
+              .required(true),
+      ).subcommand(
+          SubCommand::with_name("encrypt")
+              .arg(
+                  Arg::with_name("value")
+                      .short("v")
+                      .long("value")
+                      .help("YAML path which should be encrypted")
+                      .multiple(true)
+                      .required(true)
+                      .takes_value(true),
+              ).arg(
+                  Arg::with_name("overwrite")
+                      .short("o")
+                      .long("overwrite")
+                      .help("Overwrites input file")
+                      .conflicts_with("file")
+                      .takes_value(false),
+              ),
+      ).subcommand(SubCommand::with_name("decrypt"))
+}
 
 fn main() {
-    let matches = App::new("culper")
-        .version("0.1.0")
-        .author("Max Kiehnscherf")
-        .about("Embed crypted values in your yaml")
-        .arg(
-            Arg::with_name("file")
-                .short("f")
-                .long("file")
-                .help("Sets input file")
-                .takes_value(true)
-                .required(true),
-        ).subcommand(
-            SubCommand::with_name("encrypt")
-                .arg(
-                    Arg::with_name("value")
-                        .short("v")
-                        .long("value")
-                        .help("YAML path which should be encrypted")
-                        .multiple(true)
-                        .required(true)
-                        .takes_value(true),
-                ).arg(
-                    Arg::with_name("overwrite")
-                        .short("o")
-                        .long("overwrite")
-                        .help("Overwrites input file")
-                        .conflicts_with("file")
-                        .takes_value(false),
-                ),
-        ).subcommand(SubCommand::with_name("decrypt"))
-        .get_matches();
+    let matches = app().get_matches();
 
-    let ifile = matches.value_of("file").unwrap();
+    let ifile = matches.value_of("file").unwrap(); // clap handles this;
+
     if !Path::new(ifile).exists() {
         panic!("File not found")
     }
 
     let mut file = File::open(ifile).expect("Could not open input file");
     let mut contents = String::new();
+
     file.read_to_string(&mut contents)
         .expect("Could not read file contents");
+
     let mut yml: serde_yaml::Value =
         serde_yaml::from_str(&contents).expect("Could not parse file content");
 
