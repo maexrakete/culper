10e7d2b32d0e63f7b1a70ef89ba73410078b62d1
Mäx <max.kiehnscherf@gmail.com>
2018-11-20 22:28:13 +0100

    [fix] musl rust version

--- release-build.sh
+++ release-build.sh
@@ -6,4 +6,4 @@ if [ "$TRAVIS" = true ]; then
     chmod -R 777 .
 fi
 
-docker run -e LIBZ_SYS_STATIC=1 --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:1.30.1 cargo build --release
+docker run -e LIBZ_SYS_STATIC=1 --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:nightly cargo build --release
