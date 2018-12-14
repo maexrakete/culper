6db5a2aa878640f599a7f1272c368339c12b805a
Max Kiehnscherf <max.kiehnscherf@gmail.com>
2018-12-15 00:58:00 +0100

    fix build

--- build_publish_docker.sh
+++ build_publish_docker.sh
@@ -9,9 +9,9 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build --build-arg CULPER_VER=${tag} -t ${tagged_image_name} .
 
-if [ $(echo $tag | rg "alpha|beta|rc") ]
+if [ $(echo $tag | grep -E "(alpha|beta|rc)") ]
 then
     docker tag ${tagged_image_name} ${base_image_name}:latest
 fi
 
-docker publish ${base_image_name}
+docker push ${base_image_name}
