5689d17cf688d135a2b205a2fc1cb04f0cf4a9ef
Mäx <max.kiehnscherf@gmail.com>
2018-12-14 19:09:05 +0100

    fix broken build

--- release-build.sh
+++ release-build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-if [ "$TRAVIS" = true ]; then
-    chmod -R 777 .
-fi
-
-rm -rf target
-
 docker run -e LIBZ_SYS_STATIC=1 --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:nightly cargo build --release
